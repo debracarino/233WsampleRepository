127e5226de7014be47d07e55c87592d96ed3f64a
debracarino <dcarino@clackamas.edu>
2025-10-01 13:36:18 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+/** server.js
+ * demonstration of simple, custome web server using Express.js
+ * 
+ * Debra Carino & CS 233W students
+ * CS 233W - Week 1 Activity
+ * 10/1/2025
+ */
+
+//Import desired frameworks
+const express = require('express');
+const app = express();
+
+//set listening port value
+const PORT = 3000;
+
+//configure routing
+app.get('/', (request, response) => {
+    response.send('<h1>The Tasty Tortellini</h1><h2>Southeast PDX\'s favorite Italian bistro</h2>');
+});
+
+app.get('/menus', (request, response)=> {
+    response.send('<h1>Menus</h1><ul><li>Lunch</li><li>Dinner</li><li>Drinks</li><li>Desserts</li></ul>');
+});
+
+//connect server to desired port
+app.listen(PORT, () => {
+    console.log("Web server currently listening on port", PORT);
+});
